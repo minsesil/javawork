cf20ff1f478c0ce7e490f869c49ed9c628992cd0
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:33:36 +0900

    Create Car.java

---
+++ 07_object/src/objField02/Car.java
@@ -0,0 +1,13 @@
+package objField02;
+
+public class Car {
+	
+	//속성
+	String company = "삼성자동차";		//기본값 : null
+	String model = "SM7";			//기본값 : null
+	int maxSpeed = 300;				//변수, 기본값 : 0
+	final int MAX_SPEED = 300;		//상수, 기본값 : 0		final-상수(한번 값을 넣으면 변경할수 없음, 대문자)
+	boolean engineStart;			// 기본값 : false
+	
+
+}
