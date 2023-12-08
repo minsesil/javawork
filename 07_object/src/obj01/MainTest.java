c7e15f1e649d33dfe7544f85d74911668c2313e0
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:32:52 +0900

    Create MainTest.java

---
+++ 07_object/src/obj01/MainTest.java
@@ -0,0 +1,14 @@
+package obj01;
+
+// 실행용 클래스
+public class MainTest {
+
+	public static void main(String[] args) {
+
+		//main메소드가 있는곳에서 객체생성
+			A 		a1 = new A(); 			//A 클래스 객체 생성
+			A 		a2 = new A();			//A 클래스 객체 하나 더 생성
+//        클래스이름		
+	}
+
+}
