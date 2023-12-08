711ac924c7923f57e38c7c564bc9501cffcafc16
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:42:36 +0900

    Create MysqlDao.java

---
+++ 10_abstract/src/interface05/MysqlDao.java
@@ -0,0 +1,21 @@
+package interface05;
+
+public class MysqlDao implements DataAccessObject{
+
+	@Override
+	public void select() {
+		System.out.println("oracle db에 검색");
+	}
+	@Override
+	public void insert() {
+		System.out.println("oracle db에 삽입");
+	}	
+	@Override
+	public void update() {
+		System.out.println("oracle db에 수정");
+	}	
+	@Override
+	public void delete() {
+		System.out.println("oracle db에 삭제");
+	}	
+}
