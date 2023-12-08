b287f02545c0d27fb466f3b5d18483c6030763be
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:42:17 +0900

    Create Car.java

---
+++ 10_abstract/src/interface04/Car.java
@@ -0,0 +1,19 @@
+package interface04;
+
+public class Car {
+	Tire frontLeftTire = new HankookTire();
+	Tire frontRightTire = new KumhoTire();
+	Tire backLeftTire = new KumhoTire();
+	Tire backRightTire = new HankookTire();
+
+	void run() {
+		frontLeftTire.roll();
+		frontRightTire.roll();
+		backLeftTire.roll();
+		backRightTire.roll();
+	}
+	
+	
+	
+	
+}
