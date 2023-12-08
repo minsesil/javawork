9b1f5a0c11d90954b345d73dd7bd70f9378c0106
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:39:06 +0900

    Create Ex231027Test.java

---
+++ 09_inheritance/src/ex231027/Ex231027Test.java
@@ -0,0 +1,14 @@
+package ex231027;
+
+public class Ex231027Test {
+
+	public static void main(String[] args) {
+		
+		//핸드폰 객체생성
+		Ex231027 phone = new Ex231027();
+		//호출
+		System.out.println(phone.getNum1());
+		
+	}
+
+}
