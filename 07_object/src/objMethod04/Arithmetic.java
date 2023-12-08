29cee053481c64fbc0ea866e68ac1ed8cfdb8a55
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:33:50 +0900

    Create Arithmetic.java

---
+++ 07_object/src/objMethod04/Arithmetic.java
@@ -0,0 +1,30 @@
+package objMethod04;
+
+public class Arithmetic {
+
+//5칙연산 메소드 만들기
+		int plus(int x, int y) {
+			int re = x +y;
+			return re;
+		}
+		
+		int minus(int x, int y) {
+			return x - y;
+		}
+		
+		int multiply(int x, int y) {
+			return x * y;
+		}
+		
+		double divide(int x, int y) {
+			return (double)x/y;
+		}
+		
+		int remainder(int x, int y) {
+			return x%y;
+		}
+	
+	
+	
+
+}
