319a7fb288058001f6540f3927e8eb0341003c40
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:32:46 +0900

    Create A.java

---
+++ 07_object/src/obj01/A.java
@@ -0,0 +1,16 @@
+package obj01;
+//라이브러리 클래스 or API 클래스
+
+public class A {
+	
+	// 속성 or 필드(변수선언)
+	int var1;
+	String name;
+	boolean b1;
+	
+	//메소드(기능실행)-> main메소드가 있는곳에서 객체생성해서 사용한다
+	public void Method1() {
+		System.out.println("Method1 호출");
+	}
+
+}
