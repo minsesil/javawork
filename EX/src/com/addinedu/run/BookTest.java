847e7dea04fb479fdda819a7e3f300a94509922c
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:49:29 +0900

    Create BookTest.java

---
+++ EX/src/com/addinedu/run/BookTest.java
@@ -0,0 +1,102 @@
+package com.addinedu.run;
+
+import java.util.Scanner;
+import com.addinedu.model.vo.Book;
+
+public class BookTest {
+
+	public static void main(String[] args) {
+		
+		//객체생성
+		//복습 1.
+		
+		Book b1 = new Book();
+		
+		b1.setTitle("b1:혼자공부하는 자바");
+		b1.setAuthor("b1:신용권");
+		b1.setPrice(32000);
+		b1.setPublisher("b1:한빛미디어");
+		
+		Book b2 = new Book("b2:마이바티스 프로그래밍","이동국",25000,"에이콘");
+System.out.println();
+	
+		Book b3 = new Book();		
+		//사용자로부터 입력받아서 필드에 값저장		
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("b3:도서명 입력");
+		b3.setTitle(scan.nextLine());
+		
+		System.out.println("b3:저자 입력");
+		b3.setAuthor(scan.nextLine());
+		
+		System.out.println("b3:가격 입력");
+		b3.setPrice(scan.nextInt());
+
+		System.out.print("b3:출판사 입력");
+		b3.setPublisher(scan.nextLine());
+	
+System.out.println();
+		
+		System.out.println("b33:도서명 입력 : ");
+		String title2 = scan.nextLine();
+		
+		System.out.println("b33:저자 입력 : ");
+		String author2 = scan.nextLine();
+
+		System.out.println("b33:가격 입력 : ");
+		int price2 = scan.nextInt();
+		
+		System.out.print("b33:출판사 입력 : ");
+		String publisher2 = scan.next();
+	
+		System.out.println(b3.getTitle());
+
+
+System.out.println();
+		Book b4 = new Book(title2, author2, price2, publisher2);
+	
+		//일반 변수에 3권의 책을 입력
+		Book book1 = null;
+		Book book2 = null;
+		Book book3 = null;
+	
+		for(int i=1; i<=3; i++) {
+			System.out.println("b4:도서명 입력");
+			String title = scan.nextLine();
+			
+			System.out.println("b4:저자 입력");
+			String author = scan.nextLine();
+			
+			System.out.println("b4:가격 입력");
+			int price = scan.nextInt();
+
+			System.out.print("b4:출판사 입력");
+			String publisher = scan.nextLine();
+
+			//매번 검사
+			if(i==1)
+				book1 = new Book(title, author, price, publisher);
+			else if(i==2)
+				book2 = new Book(title,author,price,publisher);
+			else
+				book3 = new Book (title,author,price,publisher);
+			System.out.println(book1.information());
+	}
+		System.out.println(book1.information());
+		System.out.println(book2.information());
+		System.out.println(book3.information());
+		
+		//도서 제목으로 검색
+		System.out.println("검색할 책 제목");
+		String search = scan.nextLine();
+		
+		if(book1.getTitle().equals(search))
+			System.out.println(book1.information());
+		else if(book2.getTitle().equals(search))
+			System.out.println(book2.information());
+		else
+			System.out.println(book3.information());
+	}	
+		
+}
