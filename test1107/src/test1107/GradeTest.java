772da182458899fc66b02d3d1c86105005376ed4
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:58:12 +0900

    Create GradeTest.java

---
+++ test1107/src/test1107/GradeTest.java
@@ -0,0 +1,17 @@
+package test1107;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradeTest {
+
+	public static void main(String[] args) {
+		
+		GradeMenu gMenu = new GradeMenu();
+		
+		List<Integer> list = new ArrayList<Integer>();
+		System.out.println(list);
+
+	}
+
+}
