7a6b52d58b33a8b5e028fed6801f8c8a07a08f9f
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:34:43 +0900

    Create Book.java

---
+++ 08_objectArray/src/com/addinedu/model/vo/Book.java
@@ -0,0 +1,58 @@
+package com.addinedu.model.vo;
+
+public class Book {
+	
+	//도서명, 저자, 가격, 출판사
+	private String title;
+	private String author;
+	private int price;
+	private String publisher;
+	
+	//생성자
+	public Book() {
+	}
+	
+	public Book(String title, String authos,int price, String publisher) {
+		this.title = title;
+		this.author = author;
+		this.price = price;
+		this.publisher = publisher;
+	}
+
+	//setter, getter 메소드 생성
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	//모든 필드 값 반환하는 메소드
+	public String information() {
+		return title + "," + author + "," + price + "," + publisher;
+		
+	}
+	
+	
+}
