9f06ea15d5242f86feb3f75e6a7b8513a00e81d8
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:51:45 +0900

    Create TvTest.java

---
+++ EX/src/objCar05/TvTest.java
@@ -0,0 +1,32 @@
+package objCar05;
+
+public class TvTest {
+
+	public static void main(String[] args) {
+		
+		Tv tv1 = new Tv();
+
+		System.out.println("-----Tv정보----");
+		System.out.println("회사:"+tv1.model);
+		System.out.println("모델:"+tv1.model);
+		System.out.println("inch:"+tv1.inch);
+		System.out.println("--------------");
+
+		tv1.power();
+		System.out.println("현재채널:" + tv1.channelUp());
+		System.out.println("현재채널:" + tv1.channelUp());
+		System.out.println("현재채널:" + tv1.channelUp());
+		System.out.println("현재채널:" + tv1.channelDown());
+		
+		System.out.println("현재볼륨:" + tv1.volume(13));
+		System.out.println("현재볼륨:" + tv1.volume(10));
+		
+		System.out.println("현재채널:" + tv1.channelUp());
+		System.out.println("현재채널:" + tv1.channelDown());
+	
+		
+		
+
+	}
+
+}
