68813afcd7e37327e2c44689be138b7bc586388f
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:40:13 +0900

    Create Frog.java

---
+++ 09_inheritance/src/polyEx09/Frog.java
@@ -0,0 +1,21 @@
+package polyEx09;
+
+public class Frog extends Pet{
+
+	Frog() {
+		super("개구리", "진연두");
+	}
+	Frog(String color) {
+		super("개구리", color);
+	}
+	
+	@Override
+	void sound() {
+		System.out.println("개굴개굴");
+	}
+	
+	@Override
+	void pattern() {
+		System.out.println("점프를 잘한다");
+	}
+}
