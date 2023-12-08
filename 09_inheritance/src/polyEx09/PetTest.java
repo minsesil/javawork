ea6ffc45cb41792d49bce3a691166c163ccbc96f
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:40:17 +0900

    Create PetTest.java

---
+++ 09_inheritance/src/polyEx09/PetTest.java
@@ -0,0 +1,25 @@
+package polyEx09;
+
+public class PetTest {
+
+	public static void main(String[] args) {
+		Pet pet = new Dog("흰색");
+		pet.sound();
+		pet.pattern();
+		
+		String kind = pet.getKind();
+		System.out.println(kind);
+		pet.getColor();
+		pet.infor();
+		System.out.println(pet);
+		System.out.println("---------------------------");
+		
+		pet = new Frog();
+		pet.sound();
+		pet.pattern();
+		System.out.println(pet);
+		
+		
+	}
+
+}
