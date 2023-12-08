055877d60d3d73cf4fc1b3a4ea6bfffc250c1229
minsesil <148636941+minsesil@users.noreply.github.com>
2023-12-08 23:22:31 +0900

    Create Ex4_구구단.java

---
+++ 04_conditional/src/conditional01/Ex4_구구단.java
@@ -0,0 +1,39 @@
+package conditional01;
+
+public class Ex4_구구단 {
+
+	public static void main(String[] args) {
+		
+     // 중첩 for문
+      for(int i=1; i<=3; i++) {
+         for(int j=5; j<=10; j++) {
+            System.out.println("i=" + i + ", j=" + j);
+         }
+      System.out.println(); //한줄띄기(엔터)         
+      }
+      
+       System.out.println("------------------"); 
+
+      //예제-구구단 2단 출력
+      /*
+       * 2*1=2
+       * 2*2=4
+       * 2*3=6
+       * ...
+       * 2*9=18
+       * 
+       * 3*1=3
+       * 3*2=6
+       */
+      for(int dan=2; dan<=9; dan++) {
+         for(int i=1; i<=9; i++) {
+            System.out.println(dan + "*" + i + "=" + dan*i);   
+            System.out.printf("%d*%d=%2d\n",dan, i, dan*i);                        
+         }
+      System.out.println();    
+      }
+
+      
+	}
+
+}
